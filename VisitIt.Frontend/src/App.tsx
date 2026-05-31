dbcb4380cdd4f07d25cfdcdf5aa582008a81614f
nates012 <konwalia.n21@gmail.com>
2026-05-31 20:56:39 +0200

    changing the proportions of dashboard

--- VisitIt.Frontend/src/App.tsx
+++ VisitIt.Frontend/src/App.tsx
@@ -1,13 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
 import Dashboard from './admin/dashboard'; // 1. Import your dashboard component
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './styles/App.css'
-import Home from './pages/Home'
 
 function App() {
   return (
