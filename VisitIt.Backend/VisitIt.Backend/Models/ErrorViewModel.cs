ba81a9d8ef330d2ad8805d351a15bce32f5d6bff
kaniapaulina <kaniapaulina2308@gmail.com>
2026-06-02 21:15:39 +0200

    feat/backend: changed structure from mvc to webapi

--- VisitIt.Backend/VisitIt.Backend/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VisitIt.Backend.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
