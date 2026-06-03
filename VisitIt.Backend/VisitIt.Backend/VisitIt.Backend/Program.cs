416ad81c1b671460518d768f15e3f8c45e5afc13
kaniapaulina <kaniapaulina2308@gmail.com>
2026-06-03 21:41:20 +0200

    feat/backend: started login initiation, by adding user class and necessary packages

--- VisitIt.Backend/VisitIt.Backend/VisitIt.Backend/Program.cs
+++ VisitIt.Backend/VisitIt.Backend/VisitIt.Backend/Program.cs
@@ -1,5 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+using VisitIt.Backend.Data;
+using VisitIt.Backend.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentityApiEndpoints<User>().AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -13,23 +36,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowReact", policy =>
-    {
-        policy.WithOrigins("http://localhost:5173")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
-    });
-});
-
 app.UseHttpsRedirection();
-
+app.UseCors("AllowReact");
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
-
-app.UseCors("AllowReact");
