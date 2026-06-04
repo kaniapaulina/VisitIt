df671885ca1c7679a9b1c69594f8f304bb48d8c8
kaniapaulina <kaniapaulina2308@gmail.com>
2026-06-04 23:41:30 +0200

    feat/login: reworked login backend

---
+++ VisitIt.Backend/VisitIt.Backend/VisitIt.Backend/DTO/UserDto.cs
@@ -0,0 +1,30 @@
+﻿using VisitIt.Backend.Models;
+
+namespace VisitIt.Backend.DTO
+{
+    public class UserLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserRegisterDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserResponseDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role {  get; set; }
+
+        // JWT token for authentication
+        public string Token { get; set; } 
+    }
+
+  
+}
