82083afe8f6628c74dc96fd8be33210b59668ea9
nates012 <konwalia.n21@gmail.com>
2026-06-07 16:02:34 +0200

    cosmetic changes in dashboard css

--- VisitIt.Frontend/src/pages/admin/AdminDashboard.tsx
+++ VisitIt.Frontend/src/pages/admin/AdminDashboard.tsx
@@ -1,5 +1,6 @@
 import styles from './dashboard.module.css';
 import { useNavigate, NavLink, Outlet } from 'react-router-dom';
+import { FaSignOutAlt} from 'react-icons/fa';
 
 export default function AdminDashboard() {
   const navigate = useNavigate();
@@ -28,7 +29,7 @@ export default function AdminDashboard() {
             <img src="https://d1jyxxz9imt9yb.cloudfront.net/medialib/3078/image/s768x1300/IP202207_GlassFrogs_009_365211_reduced.jpg" alt="User" className={styles.userAvatar} />
             <span className={styles.userName}>Admin</span>
             <button onClick={handleLogout} className={styles.logoutBtn}>
-              Log out
+              <FaSignOutAlt /> Log out
             </button>
          </div>
       </nav>
